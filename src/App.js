87fe4ac7bc25096c9e6fc9d4f730b9a58773e8a7
Benjamin <benjaminrenny@gmail.com>
2022-08-30 11:18:03 +0100

    final build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Chat from "./components/Chat";
+
+const style = {
+  appContainer: `max-w-[720px] mx-auto text-center`,
+  sectionContainer: `flex flex-col h-[90vh] mt-10 shadow-xl border bg-gray-100 relative`,
+};
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.appContainer}>
+      <section className={style.sectionContainer}>
+        <Navbar />
+        {user ? <Chat /> : null}
+      </section>
     </div>
   );
 }
